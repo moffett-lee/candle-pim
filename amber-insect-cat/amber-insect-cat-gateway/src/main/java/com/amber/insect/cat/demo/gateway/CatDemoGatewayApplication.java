2b12d950ba30fdbc15b43e36f9af6491f575136e
liyuze <ezuylee@gmail.com>
2021-12-02 22:29:58 +0800

    feat: cat学习工程搭建

---
+++ amber-insect-cat/amber-insect-cat-gateway/src/main/java/com/amber/insect/cat/demo/gateway/CatDemoGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.amber.insect.cat.demo.gateway;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @ClassName com.amber.insect.cat.demo.gateway.CatDemoGatewayApplication
+ * @Description 网关启动器
+ * @Author Amber.L
+ * @Date 2021/12/2 22:27
+ * @Version 1.0
+ **/
+public class CatDemoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatDemoGatewayApplication.class,args);
+    }
+
+}
