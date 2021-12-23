d8c19e176a4fcc9f5551ee793fe06001daa9f383
“ext.liyuze” <ext.liyuze@jd.com>
2021-12-23 14:07:01 +0800

    spring boot 设计模式 demo

---
+++ amber-insect-design-patterns/src/main/java/com/rainze/designpatterns/RainzeDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.rainze.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainzeDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainzeDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ amber-insect-design-patterns/src/test/java/com/rainze/designpatterns/RainzeDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainze.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainzeDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
