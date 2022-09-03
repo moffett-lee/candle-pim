ff7208836026f0b54cd07d554ac7714940b38a2b
leyuze <leyuze@88.com>
2022-09-03 20:14:52 +0800

    更新版本

--- amber-insect-java-web/web/login_chk.jsp
+++
@@ -1,8 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=utf-8"
-         pageEncoding="utf-8" %>
-<%--防强值跳转 --%>
-<%
-    /*if (session.getAttribute("currentUser") == null) {
-        out.println("<script>alert('登录超时，请先登录！');window.location.href='login.jsp';</script>");
-    }*/
-%>
---
+++ java-config-file/src/main/java/com/leyuze/javaconfigfile/JavaConfigFileApplication.java
@@ -0,0 +1,13 @@
+package com.leyuze.javaconfigfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConfigFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaConfigFileApplication.class, args);
+    }
+
+}
